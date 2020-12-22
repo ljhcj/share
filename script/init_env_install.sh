5b0f0e30f79ce6afb27281f90e2d4af12ffa50d3
ljhcj <sh_ljhcj@163.com>
2020-12-22 17:35:14 +0800

    Update init_env_install.sh

--- script/init_env_install.sh
+++ script/init_env_install.sh
@@ -62,8 +62,8 @@ jdk13(){
     cd $dir && wget -V &> /dev/null || yum -y install wget
     java -version &> /dev/null && echoRed "检测到系统中有java命令，故而退出！" && rm -rf $dir && exit 1
     wget -nc https://mirrors.huaweicloud.com/java/jdk/13+33/jdk-${jdk_version}_linux-x64_bin.tar.gz && tar xf jdk-${jdk_version}_linux-x64_bin.tar.gz -C /usr/local/
-    echo 'JAVA_HOME=/usr/local/jdk-${jdk_version}' >> /etc/profile && echo 'PATH=$PATH:$JAVA_HOME/bin' >> /etc/profile && echo 'export PATH' >> /etc/profile && source /etc/profile
-    /usr/local/jdk-${jdk_version}/bin/java -version &> /dev/null && echoGreen "已完成安装，可尽情享用！" || echoYellow "可能安装有问题，请检查！" 
+    echo 'JAVA_HOME=/usr/local/jdk-13' >> /etc/profile && echo 'PATH=$PATH:$JAVA_HOME/bin' >> /etc/profile && echo 'export PATH' >> /etc/profile && source /etc/profile
+    /usr/local/jdk-13/bin/java -version &> /dev/null && echoGreen "已完成安装，可尽情享用！" || echoYellow "可能安装有问题，请检查！" 
     S && rm -rf $dir
 }
 mysql(){
